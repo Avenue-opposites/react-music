f25b774c5f8474b37013df26c60f8312e800a226
Avenue-opposites <Avenue-opposites>
2023-10-05 16:50:12 +0800

    chore:Input添加fullWidth属性

--- src/components/Inputs/Input.tsx
+++ src/components/Inputs/Input.tsx
@@ -1,26 +1,32 @@
+import clsx from 'clsx'
 import BaseInput, { InputBaseProps } from './BaseInput'
 
 interface InputProps extends InputBaseProps {
   label: string;
+  fullWidth?: boolean;
 }
 
 const Input: React.FC<InputProps> = (props) => {
   const {
     label,
     name,
+    fullWidth,
   } = props
 
   return (
-    <div className="inline-flex flex-col gap-y-2">
-      <label
-        className="
+    <div className={clsx('inline-flex flex-col', fullWidth && 'w-full', label && 'gap-y-2')}>
+      {
+        label &&
+        <label
+          className="
           block text-sm 
           font-medium text-gray-900
         "
-        htmlFor={name}
-      >
-        {label}
-      </label>
+          htmlFor={name}
+        >
+          {label}
+        </label>
+      }
       <BaseInput
         {...props}
         className="
